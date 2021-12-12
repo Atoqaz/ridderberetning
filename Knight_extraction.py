8441890d531a91345b872d7daba2046ee8f9867c
Atoqaz <atoqaz@gmail.com>
2021-12-12 16:28:16 +0100

    Get text from pdf

--- Knight_extraction.py
+++ Knight_extraction.py
@@ -25,22 +25,23 @@ from io import StringIO
 DIR = Path(__file__).parent
 
 
-def load_seachwords(filepath: str):
+def load_seachwords(filepath: str, lowercase: bool = True):
     """ Load excel file with citynames. Extract each unique city from the list. """
-    df_cities = pd.read_excel(filepath, names=["Cities"])
+    df_cities = pd.read_excel(filepath, names=["Cities"], header=None)
+    if lowercase:
+        df_cities["Cities"] = df_cities["Cities"].str.lower()
     df_cities = df_cities.drop_duplicates(subset=["Cities"])
     cities = df_cities.to_numpy().T[0]  # Convert to numpy array and transpose
     return cities
 
 
-def convert_pdf_to_txt(filepath, maxpages=0):
+def convert_pdf_to_txt(filepath: Path, maxpages: int = 0):
+    """ Read pdf, convert it to searchable text & add pagenumber to raw text. """
     rsrcmgr = PDFResourceManager()
     output_string = StringIO()
     device = TextConverter(rsrcmgr, output_string, codec="utf-8", laparams=LAParams())
     with open(filepath, "rb") as pdf_file:
         interpreter = PDFPageInterpreter(rsrcmgr, device)
-        # maxpages = 0
-        caching = True
         pagenos = set()
 
         for pagenumber, page in enumerate(
@@ -49,11 +50,11 @@ def convert_pdf_to_txt(filepath, maxpages=0):
                 pagenos,
                 maxpages=maxpages,
                 password="",
-                caching=caching,
+                caching=True,
                 check_extractable=True,
             )
         ):
-            output_string.write(f"<PDF PAGE SPLIT><Page {pagenumber}>\n")
+            output_string.write(f"<PDF PAGE SPLIT><Page>{pagenumber}>\n")
             interpreter.process_page(page)
         text = output_string.getvalue()
         device.close()
@@ -61,21 +62,29 @@ def convert_pdf_to_txt(filepath, maxpages=0):
     return text
 
 
-def search_pdf(pdf_text, searchwords):
-    pages = pdf_text.split("<PDF PAGE SPLIT>")
-    # for each page, for each word. If word matches write page number and line to excel doc
-    print(pages)
+def search_pdf(pdf_text: str, searchwords: list):
+    """ Search pages for the searchwords, and store page number if exist """
+    pages = pdf_text.split("<PDF PAGE SPLIT>")[1:]
+    location = np.empty((0, 2), str)
+    for page in pages:
+        for word in searchwords:
+            if word in page.lower():
+                pagenum = page.split("<Page>")[1].split(">")[0]
+                location = np.append(location, [[pagenum, word]], axis=0)
+    df_location = pd.DataFrame(location, columns=["Pagenumber", "MatchingWord"])
+    return df_location
 
 
 def main():
-    filepath_cities = DIR.joinpath("Bynavne.xlsx")
+    # filepath_cities = DIR.joinpath("Bynavne.xlsx")
+    filepath_cities = DIR.joinpath("sample.xlsx")
     filepath_statskalender = DIR.joinpath("Statskalender/sample.pdf")
-    filepath_statskalender = DIR.joinpath("Statskalender/Matlab Cheatsheet.pdf")
+    # filepath_statskalender = DIR.joinpath("Statskalender/Matlab Cheatsheet.pdf")
     # filepath_statskalender = DIR.joinpath("Statskalender/Statskalender 1950.pdf")
     cities = load_seachwords(filepath=filepath_cities)
     pdf_text = convert_pdf_to_txt(filepath=filepath_statskalender, maxpages=3)
-    search_pdf(pdf_text=pdf_text, searchwords=cities)
-    # print(pdf_text)
+    location = search_pdf(pdf_text=pdf_text, searchwords=cities)
+    print(location)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 from pathlib import Path
 from pdfminer.pdfpage import PDFPage
 from pdfminer.high_level import extract_text, extract_pages
@@ -10,89 +12,8 @@ filename = DIR.joinpath("Statskalender/Matlab Cheatsheet.pdf")
 filename = DIR.joinpath("Statskalender/sample.pdf")
 
 
-# Extract iterable of LTPage objects.
-# pages = extract_pages(filename)
-# print(pages)
+location = np.array([[1, 2, 3], [4, 5, 6]])
 
+test = np.append(location, [[7, 8, 9]], axis=0)
+print(test)
 
-# from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-# from pdfminer.pdfpage import PDFPage
-# from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
-# from pdfminer.layout import LAParams
-# from pdfminer.pdfdocument import PDFDocument
-# from pdfminer.pdfparser import PDFParser
-# import io
-# import os
-
-# fp = open(filename, "rb")
-# rsrcmgr = PDFResourceManager()
-# retstr = io.StringIO()
-# print(type(retstr))
-# codec = "utf-8"
-# laparams = LAParams()
-# device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-# interpreter = PDFPageInterpreter(rsrcmgr, device)
-
-# page_no = 0
-# for pageNumber, page in enumerate(PDFPage.get_pages(fp)):
-#     if pageNumber == page_no:
-#         interpreter.process_page(page)
-
-#         print(interpreter)
-
-#         # data = retstr.getvalue()
-
-#         # # with open(
-#         # #     os.path.join(
-#         # #         "Files/Company_list/0010/text_parsed/2017AR", f"pdf page {page_no}.txt"
-#         # #     ),
-#         # #     "wb",
-#         # # ) as file:
-#         # #     file.write(data.encode("utf-8"))
-#         # data = ""
-#         # retstr.truncate(0)
-#         # retstr.seek(0)
-
-#     page_no += 1
-
-
-
-
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO
-
-
-def convert_pdf_to_txt(path):
-    rsrcmgr = PDFResourceManager()
-    output_string = StringIO()
-    device = TextConverter(rsrcmgr, output_string, codec="utf-8", laparams=LAParams())
-    with open(path, "rb") as pdf_file:
-        interpreter = PDFPageInterpreter(rsrcmgr, device)
-        maxpages = 0
-        caching = True
-        pagenos = set()
-
-        for pagenumber, page in enumerate(
-            PDFPage.get_pages(
-                pdf_file,
-                pagenos,
-                maxpages=maxpages,
-                password="",
-                caching=caching,
-                check_extractable=True,
-            )
-        ):
-            interpreter.process_page(page)
-            output_string.write(f"<PDF PAGE SPLIT><{pagenumber}>\n")
-        text = output_string.getvalue()
-        print(text)
-
-        device.close()
-        output_string.close()
-    return text
-
-
-convert_pdf_to_txt(filename)
