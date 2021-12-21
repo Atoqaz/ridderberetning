ddb46a2c7998eff640915286e8fb745d86f91caf
Atoqaz <atoqaz@gmail.com>
2021-12-21 22:47:59 +0100

    Regular expression lines complete

--- test.py
+++ test.py
@@ -12,20 +12,21 @@ import re
 
 # filename = DIR.joinpath("Statskalender/Statskalender 1950-pages-100.pdf")
 # filename = DIR.joinpath("test/txt_vars_line_0.6.txt")
-# filename = DIR.joinpath("test/txt_vars_line_0.6.txt")
+filename = DIR.joinpath("test/txt_vars_line_0.6.txt")
 
 
-# with open(filename) as file:
-#     for line in file:
-#         # result = bool(re.match("^[\d]+[.][\s]", line))  # xxx
-#         # result = bool(re.match("^[—][\s]", line))  # Line
-#         result = bool(re.match("[.]$", line))  # dot
-#         if result:
-#             print(line)
+with open(filename) as file:
+    count = 0
+    for line in file:
+        # result = bool(re.search("^[\d]+[.][\s]", line))  # xxx
+        result = bool(re.search("^(â€”)[\s]", line))  # Line
+        # result = bool(re.search("[.][\\n]$", line))  # dot
+        if result:
+            count += 1
+            print(line)
 
+print(count)
 
-# html = HTMLConverter(filename,)
-# print(html)
 
 sentences = [
     "29.  1. 29. Hansen, Hans Frederik Valdemar, (D.M.11/i239),Skibsinspektør* under Grønlands Styrelse.",
@@ -39,9 +40,10 @@ sentences = [
     "Godsejer, Eskær.",
 ]
 
-for sentence in sentences:
-    # result = bool(re.match("^[\d]+[.][\s]", sentence))  # xxx
-    # result = bool(re.match("^[—][\s]", sentence))  # Line
-    result = re.match("[\.]$", sentence)  # dot
-    print(result)
+# for sentence in sentences:
+#     # result = re.search("^[\d]+[.][\s]", sentence)  # xxx
+#     result = re.findall("[\d]+[.][\s]", sentence)  # xxx all
+#     # result = re.search("^(â€”)[\s]", sentence)  # Line (Hyphen)
+#     # result = re.search("[\.][\\n]$", sentence)  # dot
+#     print(result)
 
